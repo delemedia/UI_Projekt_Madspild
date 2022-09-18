bd5a11ee5f80eb4575781e1a177e2612430dffc8
delemedia <nikk0375@stud.kea.dk>
2022-09-18 12:56:07 +0200

    Skaber en repository

---
+++ src/main/java/kea/dk/ui_index/UiIndexApplication.java
@@ -0,0 +1,13 @@
+package kea.dk.ui_index;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiIndexApplication.class, args);
+    }
+
+}
